2ea97dd2568624462a2a921c24023ba12a1b9015
Admin <admin@xinwuchan.com>
2026-09-03 01:59:01 +0800

    update

--- len.bat
+++ len.bat
@@ -6,7 +6,7 @@ git push origin main
 
 echo.
 echo Dang day code sang kho ruanchenglu1-lab/massage-bot-zhongli (Render)...
-git push https://github.com/ruanchenglu1-lab/massage-bot-zhongli.git main --force
+git push render main --force
 
 echo.
 echo XONG! Code da len va Render se tu chay.
